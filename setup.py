3cef93a0e1f868646d0228c4c98cc43f8fa12c18
Bob Pepin <bob.pepin@alexandra.dk>
2021-09-27 13:25:50 +0200

    PyPI release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sqltables",
-    version="1.2",
+    version="1.3",
     author="Bob Pepin",
     author_email="pypi@pepin.io",
     description="SQL tables as first-class objects",
