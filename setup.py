5117a4a2274a196769f580c12f93c02b26c5b4e4
Bob Pepin <bobpepin@users.noreply.github.com>
2023-02-17 10:44:09 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sqltables",
-    version="1.7",
+    version="1.8",
     author="Bob Pepin",
     author_email="pypi@pepin.io",
     description="SQL tables as first-class objects",
